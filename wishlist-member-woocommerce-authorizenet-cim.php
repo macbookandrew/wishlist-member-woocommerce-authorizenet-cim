7a303217c3841334a87fdde5529fe915f874ae03
Andrew Minion <andrew@andrewrminion.com>
2015-09-22 12:51:57 -0400

    wp-cli scaffold

---
+++ wishlist-member-woocommerce-authorizenet-cim.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Wishlist Member + Woocommerce + Authorize.net CIM
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: wishlist-member-woocommerce-authorizenet-cim
+Domain Path: /languages
+*/
+
