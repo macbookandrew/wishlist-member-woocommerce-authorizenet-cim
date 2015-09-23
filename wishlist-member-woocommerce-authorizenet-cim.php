d5581aa9e2e1a3e8a1e6ba9c76c5802f00839d69
Andrew Minion <andrew@andrewrminion.com>
2015-09-23 16:50:48 -0400

    return messages rather than echoing them

--- wishlist-member-woocommerce-authorizenet-cim.php
+++ wishlist-member-woocommerce-authorizenet-cim.php
@@ -72,9 +72,9 @@ function wlmwac_add_user_levels( $order_id ) {
     }
 
     if ( 1 == $member['success'] ) {
-        echo apply_filters( 'wlmwac_register_success', '<p>You&rsquo;ve been successfully registered. <a href="' . wp_login_url() . '">Log in</a> to access your content.</p>' );
+        return apply_filters( 'wlmwac_register_success', '<p>You&rsquo;ve been successfully registered. <a href="' . wp_login_url() . '">Log in</a> to access your content.</p>' );
     } else {
-        echo apply_filters( 'wlmwac_register_failure', '<p>We&rsquo;re sorry&hellip;something went wrong while setting up your account. Please contact us for help.</p>' );
+        return apply_filters( 'wlmwac_register_failure', '<p>We&rsquo;re sorry&hellip;something went wrong while setting up your account. Please contact us for help.</p>' );
     }
 }
 
