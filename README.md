674238869857392499db0f6665ac336e2999ec60
Andrew Minion <andrew@andrewrminion.com>
2015-09-23 15:37:55 -0400

    Squashed commit of the following:
    commit dd959695edf43cb56e6ad3b9ec0926ff2dbfed1e
    Author: Andrew Minion <andrew@andrewrminion.com>
    Date:   Wed Sep 23 15:35:33 2015 -0400
    
        update documentation
    
    commit 32a3844e3dc4060e38e8029ace2af868cbb273b9
    Author: Andrew Minion <andrew@andrewrminion.com>
    Date:   Wed Sep 23 15:18:26 2015 -0400
    
        add error logging
    
    commit d70bbcb2585bf277c4b17e64b420a36005411aab
    Author: Andrew Minion <andrew@andrewrminion.com>
    Date:   Wed Sep 23 15:18:19 2015 -0400
    
        use Yoda conditons
    
        See https://make.wordpress.org/core/handbook/best-practices/coding-standards/php/#yoda-conditions
    
    commit 2ced555ae80a7b92914465cee9adc5a4c62887cf
    Author: Andrew Minion <andrew@andrewrminion.com>
    Date:   Wed Sep 23 15:05:16 2015 -0400
    
        tweak registration success/failure messages
    
    commit 1ec36c48f59f39af69cabc869f8dc2e6999a0e2e
    Author: Andrew Minion <andrew@andrewrminion.com>
    Date:   Wed Sep 23 15:04:58 2015 -0400
    
        add support for de-registering customers upon subscription cancellation
    
    commit 5805da7ac48b39228a5666ada535a39969e0f15a
    Author: Andrew Minion <andrew@andrewrminion.com>
    Date:   Wed Sep 23 11:38:22 2015 -0400
    
        update documentation
    
    commit 8673ff94dc0e3d19770cd8b09ff3443ac4fb3da0
    Author: Andrew Minion <andrew@andrewrminion.com>
    Date:   Wed Sep 23 11:37:34 2015 -0400
    
        use a more appropriate woocommerce action hook
    
    commit 902d5d5f60c5883ce3807f4ef6f8cb887abff1f6
    Author: Andrew Minion <andrew@andrewrminion.com>
    Date:   Wed Sep 23 11:34:03 2015 -0400
    
        update documentation
    
    commit 795841694c82cd8059b3f4661705a4f1ebbbb954
    Author: Andrew Minion <andrew@andrewrminion.com>
    Date:   Wed Sep 23 11:18:44 2015 -0400
    
        clean up WLM API code slightly
    
    commit 1a8f1961e9910de63bb06d7749a67fabd3783e0a
    Author: Andrew Minion <andrew@andrewrminion.com>
    Date:   Wed Sep 23 11:18:21 2015 -0400
    
        remove now-unnecessary code
    
    commit 535f9ce7146b347dd32539fbdc45cb96ba69aa51
    Author: Andrew Minion <andrew@andrewrminion.com>
    Date:   Wed Sep 23 11:12:40 2015 -0400
    
        use WLM API to create/update members rather than posting to the URL
    
    commit 99b82b30892253def0a0cd3db144a0d9453aa73b
    Author: Andrew Minion <andrew@andrewrminion.com>
    Date:   Wed Sep 23 10:20:28 2015 -0400
    
        hook into WC completed order function to set up WishList Member access
    
    commit f01c406c083f1eb81125efdc5b78597d01086bf7
    Author: Andrew Minion <andrew@andrewrminion.com>
    Date:   Tue Sep 22 15:20:03 2015 -0400
    
        tweak settings page info and display
    
    commit 9165563299ed7c4bdd9b4164b381615b6295b095
    Author: Andrew Minion <andrew@andrewrminion.com>
    Date:   Tue Sep 22 14:21:56 2015 -0400
    
        add settings fields
    
    commit 6e4e8a83d43ebc3f398a8b8ffdd04416e21feea2
    Author: Andrew Minion <andrew@andrewrminion.com>
    Date:   Tue Sep 22 14:05:35 2015 -0400
    
        add plugin documentation
    
    commit b94720a2b5b753d610ebc060f3c3bb61487ac835
    Author: Andrew Minion <andrew@andrewrminion.com>
    Date:   Tue Sep 22 14:04:10 2015 -0400
    
        update package.json

--- README.md
+++ README.md
@@ -1,35 +1,35 @@
-=== Wishlist Member + Woocommerce + Authorize.net CIM ===
-Contributors: macbookandrew
-Donate link: https://cash.me/$AndrewRMinionDesign
-Tags: wishlist, membership, woocommerce, authorize, authorize.net
-Requires at least: 3.0.1
-Tested up to: 4.3.1
-Stable tag: 1.0
-License: GPLv2 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
+# Wishlist Member + Woocommerce + Authorize.net CIM #
+**Contributors:** macbookandrew  
+**Donate link:** https://cash.me/$AndrewRMinionDesign  
+**Tags:** wishlist, membership, woocommerce, authorize, authorize.net  
+**Requires at least:** 3.0.1  
+**Tested up to:** 4.3.1  
+**Stable tag:** 1.0  
+**License:** GPLv2 or later  
+**License URI:** http://www.gnu.org/licenses/gpl-2.0.html  
 
 This plugin integrates WishList Member, Woocommerce, and the Authorize.net CIM gateway.
 
-== Description ==
+## Description ##
 
 This plugin integrates WishList Member, Woocommerce, and the Authorize.net CIM gateway specifically, though it should work with any gateway that redirects to the Woocommerce thank-you page.
 
-== Installation ==
+## Installation ##
 
 1. Upload `wishlist-member-woocommerce-authorizenet-cim` to the `/wp-content/plugins/` directory
 1. Activate the plugin through the “Plugins” menu in WordPress
 1. In WishList Member settings, go to “Integration” and then to “Shopping Cart.” Choose “Generic” from the shoppingcart menu and press the “Set Shopping Cart” button.
 1. Create the Woocommerce products and assign the appropriate SKU numbers as instructed by WishList Member
 
-== Frequently Asked Questions ==
+## Frequently Asked Questions ##
 
-= How does it work? =
+### How does it work? ###
 
 When the Woocommerce thankyou page is loaded, this plugin checks each purchased product and adds the user to the appropriate level(s) based on the product SKU(s).
 
 When a subscription is cancelled or expires, the user is removed from the appropriate level(s).
 
-== Changelog ==
+## Changelog ##
 
-= 1.0 =
+### 1.0 ###
 * Initial version
--- wishlist-member-woocommerce-authorizenet-cim.php
+++ wishlist-member-woocommerce-authorizenet-cim.php
@@ -1,12 +1,125 @@
 <?php
 /*
 Plugin Name: Wishlist Member + Woocommerce + Authorize.net CIM
-Version: 0.1-alpha
-Description: PLUGIN DESCRIPTION HERE
-Author: YOUR NAME HERE
-Author URI: YOUR SITE HERE
-Plugin URI: PLUGIN SITE HERE
+Version: 1.0
+Description: Integrates Wishlist Member, Woocommerce, and the Authorize.net CIM gateway
+Author: AndrewRMinion Design
+Author URI: https://andrewrminion.com
+Plugin URI: http://code.andrewrminion.com/wishlist-member-woocommerce-authorize-net-cim/
 Text Domain: wishlist-member-woocommerce-authorizenet-cim
 Domain Path: /languages
 */
 
+/**
+ * Prevent direct access to this file
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+/**
+ * Hook into the payment complete action
+ */
+add_action( 'woocommerce_payment_complete', 'wlmwac_add_user_levels' );
+
+/**
+ * Update the customer’s info in WishList Member
+ */
+function wlmwac_add_user_levels( $order_id ) {
+
+    // get order info
+    $order = new WC_Order( $order_id );
+
+    // set up levels array
+    $new_levels = array();
+
+    // loop through items to get level IDs and add to the array
+    foreach ( $order->get_items() as $item ) {
+        $product = $order->get_product_from_item( $item );
+        $new_levels[] = $product->sku;
+    }
+
+    // check for existing user by email address
+    $this_user = get_user_by( 'email', $order->billing_email );
+
+    // add or update members
+    if ( $this_user ) {
+        // add these levels to an existing member
+        $member = wlmapi_update_member( $this_user->ID, array(
+            'user_email'        => $order->billing_email,
+            'Levels'            => $new_levels
+        ));
+    } else {
+        // check for username conflicts
+        if ( username_exists( $order->billing_first_name . $order->billing_last_name ) ) {
+            $username = $order->billing_first_name . $order->billing_last_name . $order_id;
+        } else {
+            $username = $order->billing_first_name . $order->billing_last_name;
+        }
+
+        // add new member with these levels
+        $member = wlmapi_add_member( array(
+            'user_login'        => $username,
+            'user_email'        => $order->billing_email,
+            'address1'          => $order->billing_address_1,
+            'address2'          => $order->billing_address_2,
+            'city'              => $order->billing_city,
+            'state'             => $order->billing_state,
+            'zip'               => $order->billing_postcode,
+            'country'           => $order->billing_country,
+            'Levels'            => $new_levels
+        ));
+    }
+
+    if ( 1 == $member['success'] ) {
+        echo apply_filters( 'wlmwac_register_success', '<p>You&rsquo;ve been successfully registered. <a href="' . wp_login_url() . '">Log in</a> to access your content.</p>' );
+    } else {
+        echo apply_filters( 'wlmwac_register_failure', '<p>We&rsquo;re sorry&hellip;something went wrong while setting up your account. Please contact us for help.</p>' );
+    }
+}
+
+/**
+ * Hook into cancelled/expired subscriptions actions
+ */
+add_action( 'cancelled_subscription', 'wlmwac_remove_user_levels', 10, 2 );
+add_action( 'subscription_end_of_prepaid_term', 'wlmwac_remove_user_levels', 10, 2 );
+add_action( 'subscription_expired', 'wlmwac_remove_user_levels', 10, 2 );
+add_action( 'subscription_put_on-hold', 'wlmwac_remove_user_levels', 10, 2 );
+add_action( 'subscription_trial_end', 'wlmwac_remove_user_levels', 10, 2 );
+add_action( 'scheduled_subscription_expiration', 'wlmwac_remove_user_levels', 10, 2 );
+
+/**
+ * Remove WLM access on subscription cancellation or expiration
+ * @param integer $user_id          ID of the user for whom the subscription was cancelled
+ * @param string  $subscription_key Subscription key for the just-cancelled subscription
+ */
+function wlmwac_remove_user_levels( $user_id, $subscription_key ) {
+
+    // get original order information
+    $order_details = WC_Subscriptions_Manager::get_subscription( $subscription_key );
+    $product = new WC_Product( $order_details['product_id'] );
+
+    // remove member from level
+    $member = wlmapi_remove_member_from_level( $product->get_sku(), $user_id );
+
+    if ( 1 == $member['success'] ) {
+        write_log( 'WishList Member: customer ' . $user_id . ' successfully removed from level ' . $product->get_sku() );
+    } else {
+        write_log( 'WishList Member: failed removing customer ' . $user_id . ' from level ' . $product->get_sku() );
+    }
+}
+
+/*
+ * Log errors to WP_DEBUG if available
+ */
+if ( ! function_exists( 'write_log' ) ) {
+    function write_log ( $log )  {
+        if ( true === WP_DEBUG ) {
+            if ( is_array( $log ) || is_object( $log ) ) {
+                error_log( print_r( $log, true ) );
+            } else {
+                error_log( $log );
+            }
+        }
+    }
+}
